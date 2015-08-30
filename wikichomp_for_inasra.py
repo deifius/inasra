639ed81ff6c249d9d0219e33d67caba7daba7ee7
dafe hughes <sunji.roaoul@gmail.com>
2015-08-30 18:09:03 -0500

    Update wikichomp_for_inasra.py
    sending to disambiguouizer with higher accuracy
    parsing ampersands and parentheses more accurately

--- wikichomp_for_inasra.py
+++ wikichomp_for_inasra.py
@@ -6,24 +6,26 @@ import random
 import urllib2
 import string
 import json
+import pdb
 
 #linkable words that are part of wikipedia boilerplate
 wiki_words_reserved = ['isbn','random article','help','issn','related changes','recent changes','info','all articles with unsourced statements','Community portal','Main page','special pages','Removed','Cite','Disclaimers','upload file','about wikipedia','talk page','categories','featured content']
 
-#TODO: develop a kickass regex which encompasses many variations of reserved words!
+#TODO: develop a kickass regex (or a bloom filter) which encompasses many variations of reserved words!
+#
 
 def disambiguouizer(ambiguous_wiki, ambiguous_term):
-	#in the event of an amiguous term, the disambiguouszer lets user select the proper meaning
+	#in the event of an amiguous term, the disambiguouizer lets user select the proper meaning
 	refertochomp = ambiguous_wiki.find('may refer to') + 18
-	endchomp = ambiguous_wiki.find('<tr>\n<td class="mbox-image"')
-	disambuslice = re.findall('title=".+">.+<.+>, .+</li>\n', ambiguous_wiki[refertochomp:-1])
+	endchomp = ambiguous_wiki.find("disambigbox") - 11
+	disambuslice = re.findall('a href=".+">.+<.+>, .+</li>\n', ambiguous_wiki[refertochomp:endchomp])
 	disambuchoices =[]
-	print endchomp
-	#print disambuslice
+	#print endchomp
 	for each in disambuslice:
+		targt = each.split('"')[1]
 		descr = each.split(',')[1].split('<')[0]
-		title = each.split('"')[1]
-		disambuchoices.append([title, descr])
+		title = each.split('"')[3]
+		disambuchoices.append([title, descr,targt])
 	print ambiguous_term + " may refer to;\n"
 	selection = 0
 	for each in disambuchoices:
@@ -32,15 +34,23 @@ def disambiguouizer(ambiguous_wiki, ambiguous_term):
 	userchoice = -1
 	while 0 >  userchoice or userchoice > selection:
 		userchoice = int(input('you choose(0-' + str(selection) + '): \t'))
-	ambiguous_term = disambuchoices[userchoice][0]
+	ambiguous_term = disambuchoices[userchoice][2].split('/')[-1]
 	acronymizer(ambiguous_term)
 
 def acronymizer(wikitarget):
-	req = urllib2.Request("http://en.wikipedia.org/wiki/%s" % wikitarget.replace(' ', '_').title(), \
+	if '(' in wikitarget:
+		properly_capped = wikitarget.split('(')[0].replace(' ', '_').title() \
+									+"("+ wikitarget.split('(')[1]
+		wikitarget = wikitarget.split('(')[0]
+	else:
+		properly_capped = wikitarget.replace(' ', '_').title()
+	properly_capped = properly_capped.replace('&','%26')
+	print "http://en.wikipedia.org/wiki/" + properly_capped + "\n\n"
+	req = urllib2.Request("http://en.wikipedia.org/wiki/" + properly_capped, \
 		              headers={'User-Agent': 'Mozilla/5.0 (Fuck You Wikipedia; Me; emdash)'})
-	print wikitarget.replace(' ', '_').title()
 	wiki_dump = urllib2.urlopen(req).read().lower()
-	if wiki_dump.count('isambiguation') > 5:
+	if wiki_dump.count('isambiguation') > 10:
+		#10 seems to be more instances of 'disambiguous' references than non disambiarticales contain
 		disambiguouizer(wiki_dump, wikitarget)
 	relevance_regex = re.compile('<a href="/wiki/.*?" title=".*?">.*?</a>')
 	relephants = re.findall(relevance_regex,wiki_dump)
@@ -68,11 +78,9 @@ def acronymizer(wikitarget):
 
 
 	for element in wiki_words_reserved:
-		#print "looking for '" + element + "' to remove"
 		for letter_dict in acronym_dict:
 			while element in letter_dict:
 				letter_dict.remove(element)
-				#print "removed!"
 	for tick in range(len(acronym_dict)):
 	    if acronym_dict[tick] == []:
 		acronym_dict[tick].append(acro_term[tick])
@@ -84,7 +92,6 @@ def acronymizer(wikitarget):
 
 	running_acronym = []
 	selection = 0
-	#print acronym_dict
 	# now generate a random acronym from the dictionary
 	# && recursify
 	for each in acronym_dict:
@@ -105,8 +112,10 @@ def acronymizer(wikitarget):
 	#	else:
 	#		print random.choice(each)
 	acronymizer(term)
+
+
 if len(sys.argv) != 2:
-	term = raw_input("what do you want to make an acronym for? ")
+	term = raw_input("\n\n\nwhat do you want to make an acronym for? ")
 else:
 	term = sys.argv[1]
 acronymizer(term)
