cacb066291d7772953e22b046608fccd15a72c29
deifius <deifius@github.com>
2015-09-19 03:15:39 -0500

    spinevisualize now fragily creates a full crossword spine provided the fieldarray paramaters are simple

--- spine_visualize.py
+++ spine_visualize.py
@@ -7,30 +7,41 @@ if xwordspine[-1] == '':
 	xwordspine.pop(-1)
 for each in range(len(xwordspine)):
 	xwordspine[each] = xwordspine[each].split('\t')
-verts = 0
-horiz = 0
+if len(xwordspine)%2 == 0:
+	verts = len(xwordspine[-1][0])
+	horiz = len(xwordspine[-2][0])
+else:
+	horiz = len(xwordspine[-1][0])
+	verts = len(xwordspine[-2][0])
+
 for eachword in range(len(xwordspine)):
 	if eachword%2 == 0:
-		for eachletter in xwordspine[eachword][0]:
-			verts += 1
-		horiz -= int(xwordspine[eachword][1])
+		verts += int(xwordspine[eachword][1])
 	if eachword%2 != 0:
-		#pdb.set_trace()
-		for eachletter in xwordspine[eachword][0]:
-			horiz += 1
-		verts -= int(xwordspine[eachword][1])
+		##pdb.set_trace()
+		horiz += int(xwordspine[eachword][1])
 xwordfield = []
 for everyletter in range(verts):
 	xwordfield.append([])
 for everyletter in range(horiz):
 	for everyvert in range(verts):
 		xwordfield[everyvert].append(' ')
-cursor = ['0','0']
+cursor = [0,0]
 for eachword in range(len(xwordspine)):
 	if eachword%2 == 0:
-		print(xwordspine[eachword])
+		for letter in xwordspine[eachword][0]:
+			xwordfield[cursor[0]][cursor[1]] = letter
+			cursor[0] += 1
+		cursor[0] -= len(xwordspine[eachword][0])
+		cursor[0] += int(xwordspine[eachword][1])
+		#pdb.set_trace()
 	if eachword%2 != 0:
-		print('bark bark')
+		for letter in xwordspine[eachword][0]:
+			xwordfield[cursor[0]][cursor[1]] = letter
+			cursor[1] += 1
+		cursor[1] -= len(xwordspine[eachword][0])
+		cursor[1] += int(xwordspine[eachword][1])
+		#pdb.set_trace()
 for eachline in xwordfield:	print(eachline)
 
 print("verts:", verts)
