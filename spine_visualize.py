076e30a58173d159c6a62ccff5e86f52d4c710c9
dafe hughes <sunji.roaoul@gmail.com>
2015-09-22 22:11:27 -0500

    Update spine_visualize.py
    Cleaned up the shell visual and added a json dump.

--- spine_visualize.py
+++ spine_visualize.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 import pdb
+import json
+
 # TODO : populate the field with the words
 
-xwordspine = open('inasrafieldtest.txt').read().split('\n')
+xwordspine = open('inasrafieldtest.txt').read().replace(' ','').split('\n')
 if xwordspine[-1] == '':
 	xwordspine.pop(-1)
 for each in range(len(xwordspine)):
 	xwordspine[each] = xwordspine[each].split('\t')
 if len(xwordspine)%2 == 0:
-	verts = len(xwordspine[-1][0])
-	horiz = len(xwordspine[-2][0])
+	verts = len(xwordspine[-1][0])+1
+	horiz = len(xwordspine[-2][0])+1
 else:
-	horiz = len(xwordspine[-1][0])
-	verts = len(xwordspine[-2][0])
+	horiz = len(xwordspine[-1][0])+1
+	verts = len(xwordspine[-2][0])+1
 
 for eachword in range(len(xwordspine)):
 	if eachword%2 == 0:
@@ -20,6 +22,7 @@ for eachword in range(len(xwordspine)):
 	if eachword%2 != 0:
 		##pdb.set_trace()
 		horiz += int(xwordspine[eachword][1])
+#approximate size of grid sized to contain a spine  
 xwordfield = []
 for everyletter in range(verts):
 	xwordfield.append([])
@@ -30,6 +33,7 @@ cursor = [0,0]
 for eachword in range(len(xwordspine)):
 	if eachword%2 == 0:
 		for letter in xwordspine[eachword][0]:
+			#pdb.set_trace()
 			xwordfield[cursor[0]][cursor[1]] = letter
 			cursor[0] += 1
 		cursor[0] -= len(xwordspine[eachword][0])
@@ -42,7 +46,16 @@ for eachword in range(len(xwordspine)):
 		cursor[1] -= len(xwordspine[eachword][0])
 		cursor[1] += int(xwordspine[eachword][1])
 		#pdb.set_trace()
-for eachline in xwordfield:	print(eachline)
+print('')
+for eachline in xwordfield:
+	line = ' '
+	for each in eachline:
+		line = line + ' ' + each
+	print(line)
 
-print("verts:", verts)
-print('horiz', horiz)
+writio = open('xwordspine.json', 'w')
+writio.write(json.dumps(xwordfield))
+writio.close()
+#pdb.set_trace()
+#print("verts:", verts)
+#print('horiz', horiz)
