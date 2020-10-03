ee92e20c27de70242de1bf6ebf1cd910b879fc18
Luke Dennis <luke@lukifer.net>
2020-10-02 21:07:50 -0600

    DOM rendering

--- public/index.js
+++ public/index.js
@@ -1,18 +1,64 @@
-function render(wordgrid) {
-	const words = wordgrid.map(function(line) { return line.join("") }).join("\n");
-	document.getElementById("inasra").innerHTML = words;
-	document.getElementById("inasra2").innerHTML = words;
+
+function makeWord(isVertical, word, coords) {
+	const size = 12;
+	const div = document.createElement("div");
+	div.innerHTML += `<span>${word.split("").join("</span><span>")}</span>`;
+	div.className = `word${isVertical ? ' vertical' : ''}`;
+	div.style.left = `${size*coords[0]}px`;
+	div.style.top  = `${size*coords[1]}px`;
+	return div;
+}
+
+function render(xwords, ywords) {
+	const inasra = document.getElementById("inasra");
+	Object.entries(xwords).forEach(entry => inasra.appendChild(makeWord(false, ...entry)));
+	Object.entries(ywords).forEach(entry => inasra.appendChild(makeWord(true, ...entry)));
+}
+
+function parseLetterGrid(grid, invert) { // grid is an array of arrays containing characters
+	let out = {};
+	grid.forEach((line, y) => {
+		let current = "";
+		let coords = [];
+		line.forEach((char, x) => {
+			if(char === " ") {
+				if(current === "") return; // continue
+				if(current.length > 1) {
+					out[current] = coords;
+				}
+				current = "";
+			} else { // char !== " "
+				if(current === "") {
+					coords = invert ? [y, x] : [x, y];
+				}
+				current += char;
+			}
+		});
+	});
+	return out;
+}
+
+function flipLetterGridAxis(grid) {
+	return grid.reduce((arr, row, n) => {
+		let newRow = [];
+		for(let i = 0; i < grid.length; i++) {
+			newRow.push(grid[i][n]);
+		}
+		arr.push(newRow);
+		return arr;
+	}, []);
 }
 
 function fetchwords() {
 	fetch("/wordgrid.json").then(async function(response) {
 		var json = JSON.parse(await response.text());
-		//render(json);
+		const xwords = parseLetterGrid(json);
+		const ywords = parseLetterGrid(flipLetterGridAxis(json), true);
+		render(xwords, ywords);
 	});
-	fetch("/ipuz.json").then(async function(response) {
-		var json = JSON.parse(await response.text());
-		console.log(json);
-	});
+	// fetch("/ipuz.json").then(async function(response) {
+	// 	var json = JSON.parse(await response.text());
+	// });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
