f015d1152d78e74bcc326af21fe81108fdf38c91
deifius <sunji.rououl@gmail.com>
2021-01-16 01:57:30 -0700

    some test html

--- xword2html.py
+++ xword2html.py
@@ -31,6 +31,6 @@ footer = '</html>\n</body>'
 
 print(header)
 #print(tabulate(this, tablefmt='html'))
-print(re.sub('>([\S\s])<', r'><button>\1</button><',tabulate(this, tablefmt='html')))
+print(re.sub('>([\S])<', r'><button>\1</button><',tabulate(this, tablefmt='html')))
 
 print(footer)
