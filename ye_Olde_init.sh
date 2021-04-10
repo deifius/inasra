1f5121860d959b44353e6e3e0ac75c501668ff52
Luke Dennis <ldennis@homebound.com>
2021-04-10 12:52:53 -0600

    adding content/links/etc to JSON output

--- ye_Olde_init.sh
+++ ye_Olde_init.sh
@@ -10,8 +10,9 @@ echo "$@"
 
 test -f "acronym/links/$@" || ./WikiChomp.NU.py "$@"; clear
 ./Acronymizer.NU.py "$@"
-mkdir -p users/$USER/$@/$next_word
+next_word_dir = users/$USER/$@/$next_word
+mkdir -p $next_word_dir
 #python3 xword2html.py xwordspine.json > users/$USER/$@/$next_word/xword.html
-python3 xword2jsonhtml.py xwordspine.json > users/$USER/$@/$next_word/xword.json.html
+python3 xword2jsonhtml.py xwordspine.json $next_word_dir > $next_word_dir/xword.json.html
 #echo $next_word
 #./ye_Olde_init.sh $next_word
