159051451359b22dc7001dd6955fcff7f1bdec2f
deifius <sunji.rououl@gmail.com>
2021-06-01 23:22:48 -0600

    clues be here is handling slightly better, and restful too

--- CluesBeHere.py
+++ CluesBeHere.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 import json
 from pdb import set_trace
-#from os import system
 from sys import argv
 #import subprocess
 from glob import glob
-from time import sleep
-from os import system
 
 # Yo I receive a coordinate in X Y format on the xwordspine board
 # I construct a list of regular expressions for legal word configurations which intersect the coordinate provided
@@ -15,14 +12,16 @@ from os import system
 
 X = int(argv[1])
 Y = int(argv[2])
-print(str(X) + ',' + str(Y))
-#set_trace()
-# then a miracle happens, as described below😘️
-#set_trace()
+boards_with_this_space = []
+with open('xwordspine.json') as bookit:
+		board = json.loads(bookit.read())
+		if board[X][Y] != " ":
+			print("got to handle this")
+			exit(1)
 
 for eachPossibleMove in glob('.NextMoves/*'):
 	with open(eachPossibleMove) as possiboard: MTclue = json.loads(possiboard.read())
-	#set_trace()
 	if MTclue[X][Y] != " ":
-		#for e in MTclue: print(" ".join(e))
-		print(str(eachPossibleMove))
+				boards_with_this_space.append(str(eachPossibleMove))
+
+print(json.dumps(boards_with_this_space))
