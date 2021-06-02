18247dc6db6b350147285dcd87e0338876c4cf4b
deifius <sunji.rououl@gmail.com>
2021-06-02 00:05:26 -0600

    really committing to this whiptail ui, but sort of bummed the library does not offer color control

---
+++ Start_New_inasra.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from whiptail import Whiptail
+import json
+from os import system
+def main():
+    new_adventure = Whiptail()
+    new_adventure.title = "inasra welcomes you"
+    new_adventure.backtitle = "lots of stuff eventually"
+    spinehead = new_adventure.inputbox('what will you offer to inasra?')
+    system('./init_scrrrrp.sh ' + spinehead[0])
+if __name__ == "__main__": main()
