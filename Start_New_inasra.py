0c808523757625f0a4dc677bb0737c6d7106ea24
deifius <sunji.rououl@gmail.com>
2021-06-02 00:07:02 -0600

    just a quality comment about sanitizing methods

--- Start_New_inasra.py
+++ Start_New_inasra.py
@@ -8,5 +8,6 @@ def main():
     new_adventure.title = "inasra welcomes you"
     new_adventure.backtitle = "lots of stuff eventually"
     spinehead = new_adventure.inputbox('what will you offer to inasra?')
+    #this is where all the good sanitizing and CAP permutation should go
     system('./init_scrrrrp.sh ' + spinehead[0])
 if __name__ == "__main__": main()
