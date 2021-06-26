0e29431350b2df2d498ec1f9a7bbd1d6c33f4b24
Luke Dennis <ldennis@homebound.com>
2021-06-25 18:35:41 -0600

    starter test

---
+++ tests/test.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+import sys
+import inspect
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+import Start_New_inasra
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_createdirs(self):
+        Start_New_inasra.directory_initializer()
+        self.assertTrue(os.path.isdir('acronym/summary'))
+        self.assertFalse(os.path.isdir('i/am/not/a/dir'))
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
