7bd47f4f67872c25950a3c097f4a8d41096885f3
Luke Dennis <ldennis@homebound.com>
2021-07-10 14:36:52 -0600

    more logic, converting to tuples, tests

--- tests/test.py
+++ tests/test.py
@@ -8,6 +8,7 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import Start_New_inasra
+import lambda_experiment as ws
 
 class TestStringMethods(unittest.TestCase):
 
@@ -29,6 +30,36 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+    def test_col(self):
+        wordspace = (('h', 'i'), (' ', 'f'))
+        self.assertEqual(ws.getColumn(wordspace, 1), ('i', 'f'))
+        self.assertEqual(ws.flipWordspace(wordspace), (('h', ' '), ('i', 'f')))
+        self.assertEqual(ws.offAxis('y'), 'x')
+        self.assertEqual(ws.offAxisVal('y', 5, 10), 5)
+
+    def test_wordspace(self):
+        wordspace = (
+            ('h', 'e', 'w'),
+            ('i', '' , '' ),
+            ('p', '' , '' ),
+            ('s', '' , '' ),
+        )
+        self.assertEqual(ws.getWordspaceLen(wordspace, 'x'), 3)
+        self.assertEqual(ws.getWordspaceLen(wordspace, 'y'), 4)
+
+        self.assertEqual(ws.getLine(wordspace, 0, 2, 'y'), ('h', 'i', 'p', 's')) #p
+        self.assertEqual(ws.getLine(wordspace, 1, 0, 'x'), ('h', 'e', 'w'))      #e
+
+        self.assertEqual(ws.getNeighborIndexes(wordspace, 0, 0, 'y'), (0, 1))    #h
+        self.assertEqual(ws.getNeighborIndexes(wordspace, 0, 2, 'y'), (1, 2, 3)) #p
+        self.assertEqual(ws.getNeighborIndexes(wordspace, 0, 3, 'y'), (2, 3))    #s
+        self.assertEqual(ws.getNeighborIndexes(wordspace, 0, 0, 'x'), (0, 1))    #h
+        self.assertEqual(ws.getNeighborIndexes(wordspace, 2, 0, 'x'), (1, 2))    #w
+
+        # expectedRegexes = ('p.{1,2}')
+        # self.assertEqual(ws.getRegexesForLetter(wordspace, 2, 0, 'x'), expectedRegexes)
+        # self.assertEqual(ws.getRegexesForLetter(wordspace, 0, 2, 'y'), expectedRegexes)
 
 if __name__ == '__main__':
     unittest.main()
