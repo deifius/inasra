fae4c7099efa5dea6fb1b401e5e21c07e9a146a5
deifius <sunji.roaoul@gmail.com>
2022-12-22 22:18:01 -0700

    CLues Be here now produces pseudo regex

--- CluesBeHere.py
+++ CluesBeHere.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-import json
+import json, re
 from pdb import set_trace
 from sys import argv
-#import subprocess
 from glob import glob
-
-
 
 description = '''
  Yo I receive a coordinate in X Y format on the xwordspine board
@@ -15,26 +12,63 @@ I then compare the list to the freqygoodword list and return any matches
 a reference to a file in .NextMoves/
 '''
 
+def rotate(board):
+	return [list(row) for row in list(zip(*board))]
+
 def show_cross(zoard, position):
 	board = list(map(list, zoard))
 	for row in enumerate(board):
-		#print(f'row:{row}')
 		for col in enumerate(row[1]):
-				if abs(col[0] - position[0]) > 1:
-					if abs(row[0] - position[1]) > 1:
-						board[col[0]][row[0]] = '+'
+			if abs(col[0] - position[0]) > 1:
+				if abs(row[0] - position[1]) > 1:
+					board[col[0]][row[0]] = '+'
+			if abs(row[0] - position[1]) == 0:
+				if board[col[0]][row[0]] == ' ':
+					board[col[0]][row[0]] = '.'
+			if abs(col[0] - position[0]) == 0:
+				if board[col[0]][row[0]] == ' ':
+					board[col[0]][row[0]] = '.'
 				if abs(row[0] - position[1]) == 0:
-					if board[col[0]][row[0]] == ' ':
-						board[col[0]][row[0]] = '.'
-				if abs(col[0] - position[0]) == 0:
-					if board[col[0]][row[0]] == ' ':
-						board[col[0]][row[0]] = '.'
-					if abs(row[0] - position[1]) == 0:
-						board[col[0]][row[0]] = '*'
+					board[col[0]][row[0]] = '*'
 	for e in board: e
 	return board
 
+def get2planes(zoard, position):
+	'''i return two planes along the axes of the position'''
+	board = show_cross(zoard, position)
+	horiz = rotate(board[position[0]-1:position[0]+2])
+	board = rotate(board)
+	vert = rotate(board[position[1]-1:position[1]+2])
+	return vert, horiz
 
+def toss_non_viable(board):
+	for e in enumerate(board):
+		if e[1][1] == "*": break
+		if e[1][0].isalnum():
+			if not e[1][1].isalnum():
+				board[e[0]][1] = '+'
+		if e[1][2].isalnum():
+			if not e[1][1].isalnum():
+				board[e[0]][1] = '+'
+	board.reverse()
+	for e in enumerate(board):
+		if e[1][1] == "*": break
+		if e[1][0].isalnum():
+			#print("letter on top")
+			if not e[1][1].isalnum():
+				#print(f"{'.'.join(e[1])}no letter inline: nonviable")
+				board[e[0]][1] = '+'
+				#print(f'board length:{len(board)}')
+		if e[1][2].isalnum():
+			if not e[1][1].isalnum():
+				board[e[0]][1] = '+'
+	board.reverse()
+	board = rotate(board)[1]
+	while '+' in board:
+		if board.index('+') > board.index('*'):
+			board = board[:board.index('+')]
+		else: board = board[board.index('+')+1:]
+	return ''.join(board) # longest possible regex in this place
 
 def main():
 	X,Y = int(argv[1]), int(argv[2])
